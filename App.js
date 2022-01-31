be5e3ea2f5cbd40bcf98aa10266079bbe06ac934
JaredMartin0351 <jaredmartin0351@gmail.com>
2022-01-31 12:01:52 -0600

    feat: added language to app.js to reply to the user
    Added custom language to app.js to reply to the user

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ const app = express()
 //simple Hello World App
 
 const APP_PORT = process.env.APP_PORT || 3000; app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello to Jared and the rest of the world out there!')
 }); app.listen(APP_PORT, () => {
     console.log(`Running app at port:${APP_PORT}`)
 })
