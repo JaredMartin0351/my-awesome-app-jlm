39d9efcb26958759c0037661717da380b5896b52
JaredMartin0351 <jaredmartin0351@gmail.com>
2022-01-31 15:17:55 -0600

    Fix: pushing to fix an error

--- src/app.test.js
+++ src/app.test.js
@@ -7,4 +7,4 @@ document.documentElement.innerHTML = html.toString();
 test('hello world text', () => {
     const linkElement = document.querySelector('h2');
     expect(linkElement.textcontent).toBe('Hello to Jared Martin and the rest of the world out there!');
-});
+});;
