584e7cd8334d813c48f37bcb42cb14905dd71073
JaredMartin0351 <jaredmartin0351@gmail.com>
2022-01-31 15:12:35 -0600

    Fix: change to app test, corrected naming issue

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ const app = express()
 
 const APP_PORT = process.env.APP_PORT || 3000; app.get('/', (req, res) => {
 
-    res.send('<h2>Hello Jared! Nice to see you!</h2>')
+    res.send('<h2>Hello to Jared Martin and the rest of the world out there!</h2>')
 
 }); app.listen(APP_PORT, () => {
     console.log(`Running app at port:${APP_PORT}`)
--- src/app.test.js
+++ src/app.test.js
@@ -6,5 +6,5 @@ document.documentElement.innerHTML = html.toString();
 
 test('hello world text', () => {
     const linkElement = document.querySelector('h2');
-    expect(linkElement.textcontent).toBe('Hello to Cash and the rest of the world out there!');
+    expect(linkElement.textcontent).toBe('Hello to Jared Martin and the rest of the world out there!');
 });
