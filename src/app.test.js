96e776e39e38e4514c524babb5be374da199f6e3
JaredMartin0351 <jaredmartin0351@gmail.com>
2022-02-01 10:27:00 -0600

    Feat: added page for testing

--- src/app.test.js
+++ src/app.test.js
@@ -6,5 +6,5 @@ document.documentElement.innerHTML = html.toString();
 
 test('hello world text', () => {
     const linkElement = document.querySelector('h2');
-    expect(linkElement.textcontent).toBe('Hello to Jared and the rest of the world out there!');
+    expect(linkElement.textContent).toBe('Hello to Jared and the rest of the world out there!');
 });
